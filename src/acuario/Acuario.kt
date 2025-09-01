896bd42e092d41e0fd6c9ddd32e2c4e35d02eb76
Alfredo <alfredont1088@gmail.com>
2025-08-31 19:46:59 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/Acuario.kt
@@ -0,0 +1,27 @@
+package acuario
+
+open class Acuario(open var largo: Int = 100,open var ancho: Int = 20, open var alto: Int = 40) {
+    constructor(numeroDePeces: Int): this(){
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+
+    open var volumen: Int
+        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+        set(valor) {
+            alto = (valor * 1000) / (ancho * largo)
+        }
+
+    open var agua: Double = 0.0
+        get() = volumen * 0.9
+
+    open val forma = "rectángulo"
+
+    fun imprimirTamano() {
+        println(forma)
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm ")
+        println("Volumen: $volumen l Agua: $agua l (${agua/volumen*100.0}% lleno)")
+    }
+}
